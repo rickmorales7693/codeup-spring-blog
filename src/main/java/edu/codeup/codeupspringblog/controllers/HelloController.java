3cf9429a4a3b74a8a2558af756d7a8adf01b4469
Rick Morales <rick.morales7693@gmail.com>
2023-09-14 14:43:34 -0500

    added more mapping

--- src/main/java/edu/codeup/codeupspringblog/controllers/HelloController.java
+++ src/main/java/edu/codeup/codeupspringblog/controllers/HelloController.java
@@ -2,6 +2,7 @@ package edu.codeup.codeupspringblog.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -11,6 +12,26 @@ public class HelloController {
     @ResponseBody
     public String helloWorld() {
         return "Hello, World!";
+    }
+
+    @GetMapping("/hello/{name}")
+    @ResponseBody
+    public String helloName(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
+
+    @GetMapping("/distance/{num}")
+    @ResponseBody
+    public String distanceFromTwelve(@PathVariable int num) {
+        int distanceFromTwelve = Math.abs(12 - num);
+        return String.format("The distance from 12 is %d", distanceFromTwelve);
+    }
+
+    @GetMapping("/distance/{num1}/{num2}")
+    @ResponseBody
+    public String distanceBetweenNumbers(@PathVariable int num1, @PathVariable int num2) {
+        int distance = Math.abs(num1 - num2);
+        return String.format("%s is %s digits away from %s", num1, distance, num2);
     }
 
 }
